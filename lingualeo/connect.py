76ae6b12dacef51ee399d4163c4f7e5ddac9589b
Victor Khaustov <3192677+vi3itor@users.noreply.github.com>
2019-02-22 20:01:21 +0900

    Reorganized download classes and moved to a different module

--- lingualeo/connect.py
+++ lingualeo/connect.py
@@ -1,13 +1,22 @@
 import requests
 import pickle
+import socket
+import urllib.error
 import os
 
+from aqt.qt import *
+from . import utils
 
-class Lingualeo:
-    def __init__(self, email, password, cookies_path=None):
+
+class Lingualeo(QObject):
+    Error = pyqtSignal(str)
+
+    def __init__(self, email, password, cookies_path=None, parent=None):
+        QObject.__init__(self, parent)
         self.email = email
         self.password = password
         self.cj = requests.cookies.RequestsCookieJar()
+        self.msg = ''
         if cookies_path:
             self.cookies_path = cookies_path
             if not os.path.exists(cookies_path):
@@ -21,65 +30,69 @@ class Lingualeo:
                     # TODO: narrow exception clause (FileNotFound, pickle.UnpicklingError, etc)
                     self.cj = requests.cookies.RequestsCookieJar()
 
-    def auth(self):
-        url = 'https://api.lingualeo.com/api/login'
-        values = {'email': self.email, 'password': self.password}
-        content = self.get_content(url, values)
-        self.save_cookies()
-        return content
-
-    def is_authorized(self):
-        url = 'https://api.lingualeo.com/api/isauthorized'
-        status = self.get_content(url, None)['is_authorized']
-        return status
-
-    def get_page(self, page_number):
-        url = 'https://lingualeo.com/ru/userdict/json'
-        values = {'filter': 'all', 'page': page_number}
-        return self.get_content(url, values)['userdict3']
-
-    def get_page_by_group_id(self, group_id, page_number):
-        """
-        Get the words of a particular user dictionary (wordset)
-        """
-        url = 'https://lingualeo.com/ru/userdict/json'
-        values = {'filter': 'all', 'page': page_number, 'groupId': group_id}
-        return self.get_content(url, values)['userdict3']
-
-    def get_content(self, url, values):
-        r = requests.get(url, params=values, cookies=self.cj)
-        self.cj.update(r.cookies)
-        return r.json()
-
-    # TODO: Measure http vs https speed and
-    #  consider adding 'http' option to config
-
-    # TODO: Add processing of http status codes and raise an exception,
-    #  see: http://docs.python-requests.org/en/master/user/quickstart/#response-status-codes
-
-    def save_cookies(self):
-        if hasattr(self, 'cookies_path'):
-            with open(self.cookies_path, 'wb+') as f:
-                pickle.dump(self.cj, f)
+    def get_connection(self):
+        try:
+            if not self.is_authorized():
+                status = self.auth()
+                if status['error_msg']:
+                    self.msg = status['error_msg']
+        except requests.exceptions.RequestException:
+            self.msg = "Can't authorize. Check your internet connection."
+        except ValueError:
+            self.msg = "Error! Possibly, invalid data was received from LinguaLeo"
+        except Exception as e:
+            # TODO improve exception handling
+            self.msg = "There's been an unexpected error. Sorry about that! " + str(e.args)
+        if self.msg:
+            self.Error.emit(self.msg)
+            self.msg = ''
+            return False
+        return True
 
     def get_wordsets(self):
         """
         Get user's dictionaries (wordsets), including default ones,
         and return those, that are not empty
         """
-        url = 'https://lingualeo.com/ru/userdict3/getWordSets'
-        all_wordsets = self.get_content(url, None)["result"]
-        wordsets = []
-        for wordset in all_wordsets:
-            # Add only non-empty dictionaries
-            if wordset['countWords'] != 0:
-                wordsets.append(wordset.copy())
-        self.save_cookies()
+        if not self.get_connection():
+            return None
+        try:
+            url = 'https://lingualeo.com/ru/userdict3/getWordSets'
+            all_wordsets = self.get_content(url, None)["result"]
+            wordsets = []
+            for wordset in all_wordsets:
+                # Add only non-empty dictionaries
+                if wordset['countWords'] != 0:
+                    wordsets.append(wordset.copy())
+            self.save_cookies()
+            if not wordsets:
+                self.msg = 'No user dictionaries found'
+        except (requests.exceptions.RequestException, socket.error):
+            self.msg = "Can't get dictionaries. Check your internet connection."
+        except ValueError:
+            self.msg = "Error! Possibly, invalid data was received from LinguaLeo"
+        if self.msg:
+            self.Error.emit(self.msg)
+            self.msg = ''
+            return None
         return wordsets
 
-    def get_words(self, wordsets=None):
-        words = self.get_words_by_wordsets(wordsets) if wordsets else self.get_all_words()
-        self.save_cookies()
+    def get_words_to_add(self, wordsets=None):
+        if not self.get_connection():
+            return None
+        try:
+            words = self.get_words_by_wordsets(wordsets) if wordsets else self.get_all_words()
+            self.save_cookies()
+        except requests.exceptions.RequestException:
+            self.msg = "Can't download words. Check your internet connection."
+        except ValueError:
+            self.msg = "Error! Possibly, invalid data was received from LinguaLeo"
+
+        if self.msg:
+            self.Error.emit(self.msg)
+            self.msg = ''
+            return None
+
         return words
 
     def get_all_words(self):
@@ -119,6 +132,50 @@ class Lingualeo:
                 periods = self.get_page_by_group_id(group_id, page_number)
         return unique_words
 
+    def save_cookies(self):
+        if hasattr(self, 'cookies_path'):
+            with open(self.cookies_path, 'wb+') as f:
+                pickle.dump(self.cj, f)
+
+    # Low level methods
+    #########################
+
+    def auth(self):
+        url = 'https://api.lingualeo.com/api/login'
+        values = {'email': self.email, 'password': self.password}
+        content = self.get_content(url, values)
+        self.save_cookies()
+        return content
+
+    def is_authorized(self):
+        url = 'https://api.lingualeo.com/api/isauthorized'
+        status = self.get_content(url, None)['is_authorized']
+        return status
+
+    def get_page(self, page_number):
+        url = 'https://lingualeo.com/ru/userdict/json'
+        values = {'filter': 'all', 'page': page_number}
+        return self.get_content(url, values)['userdict3']
+
+    def get_page_by_group_id(self, group_id, page_number):
+        """
+        Get the words of a particular user dictionary (wordset)
+        """
+        url = 'https://lingualeo.com/ru/userdict/json'
+        values = {'filter': 'all', 'page': page_number, 'groupId': group_id}
+        return self.get_content(url, values)['userdict3']
+
+    def get_content(self, url, values):
+        r = requests.get(url, params=values, cookies=self.cj)
+        self.cj.update(r.cookies)
+        return r.json()
+
+    # TODO: Measure http vs https speed and
+    #  consider adding 'http' option to config
+
+    # TODO: Add processing of http status codes and raise an exception,
+    #  see: http://docs.python-requests.org/en/master/user/quickstart/#response-status-codes
+
 
 def is_word_exist(check_word, words):
     """
@@ -131,3 +188,54 @@ def is_word_exist(check_word, words):
         if word['word_id'] == check_word['word_id']:
             return True
     return False
+
+
+class Download(QThread):
+    Length = pyqtSignal(int)
+    Counter = pyqtSignal(int)
+    FinalCounter = pyqtSignal(int)
+    Word = pyqtSignal(dict)
+    Error = pyqtSignal(str)
+
+    def __init__(self, words, parent=None):
+        QThread.__init__(self, parent)
+        self.words = words
+
+    def run(self):
+        self.Length.emit(len(self.words))
+        self.add_separately()
+
+    def add_separately(self):
+        """
+        Divides downloading and filling note to different threads
+        because you cannot create SQLite objects outside the main
+        thread in Anki. Also you cannot download files in the main
+        thread because it will freeze GUI
+        """
+        counter = 0
+        problem_words = []
+
+        for word in self.words:
+            self.Word.emit(word)
+            try:
+                utils.send_to_download(word, self)
+            except (urllib.error.URLError, socket.error):
+                problem_words.append(word.get('word_value'))
+            counter += 1
+            self.Counter.emit(counter)
+        self.FinalCounter.emit(counter)
+
+        # TODO: save problem words in json format to user_files folder
+        #  and ask user to retry downloading problem words
+
+        if problem_words:
+            self.problem_words_msg(problem_words)
+
+    def problem_words_msg(self, problem_words):
+        error_msg = ("We weren't able to download media for these "
+                     "words because of broken links in LinguaLeo "
+                     "or problems with an internet connection: ")
+        for problem_word in problem_words[:-1]:
+            error_msg += problem_word + ', '
+        error_msg += problem_words[-1] + '.'
+        self.Error.emit(error_msg)
--- lingualeo/gui.py
+++ lingualeo/gui.py
@@ -2,9 +2,6 @@ import locale
 import sys
 
 import platform as pm
-import socket
-import urllib.error
-import requests.exceptions
 
 from aqt import mw
 from aqt.utils import showInfo
@@ -137,6 +134,9 @@ class PluginWindow(QDialog):
 
         self.allow_to_close(True)
 
+# Button clicks handlers and overridden events
+###################################################
+
     def loginButtonClicked(self):
         self.allow_to_close(False)
         # Read login and password
@@ -167,7 +167,7 @@ class PluginWindow(QDialog):
         self.logoutButton.setEnabled(False)
         self.set_download_form_enabled(False)
 
-        delattr(self, 'authorization')
+        delattr(self, 'lingualeo')
         utils.clean_cookies()
         self.config['stayLoggedIn'] = False
         utils.update_config(self.config)
@@ -183,7 +183,7 @@ class PluginWindow(QDialog):
 
     def wordsetButtonClicked(self):
         self.allow_to_close(False)
-        wordsets = self.authorization.get_wordsets()
+        wordsets = self.lingualeo.get_wordsets()
         if wordsets:
             self.set_download_form_enabled(False)
             wordset_window = WordsetsWindow(wordsets)
@@ -191,13 +191,52 @@ class PluginWindow(QDialog):
             wordset_window.Cancel.connect(self.set_download_form_enabled)
             wordset_window.exec_()
 
+    def reject(self):
+        """
+        Override reject event to handle Escape key press correctly
+        """
+        self.close()
+
+    def closeEvent(self, event):
+        """
+        Override close event to safely close plugin
+        """
+        if hasattr(self, 'threadclass') and not self.threadclass.isFinished():
+            qm = QMessageBox()
+            answer = qm.question(self, '', "Are you sure you want to stop downloading?",
+                                 qm.Yes | qm.Cancel, qm.Cancel)
+            if answer == qm.Yes and not self.threadclass.isFinished():
+                self.threadclass.terminate()
+            elif answer == qm.Cancel:
+                event.ignore()
+                return
+        # Delete attribute before closing to allow running the plugin again
+        delattr(mw, 'lingualeoanki')
+        if not self.checkBoxStayLoggedIn.checkState():
+            utils.clean_cookies()
+        mw.reset()
+
+    def downloadFinished(self):
+        if hasattr(self, 'wordsFinalCount'):
+            showInfo("%d words from LinguaLeo have been processed" % self.wordsFinalCount)
+            delattr(self, 'wordsFinalCount')
+        self.set_download_form_enabled(True)
+        self.logoutButton.setEnabled(True)
+        self.progressLabel.hide()
+        self.progressBar.hide()
+        self.allow_to_close(True)
+        mw.reset()
+
+# Functions for connecting to lingualeo and downloading words
+###########################################################
+
     def authorize(self, login, password, cookies_path=None):
         """
-        Creates authorization object and connects to lingualeo website
+        Creates lingualeo object and connects to the website
         """
-        self.authorization = Authorization(login, password, cookies_path)
-        self.authorization.Error.connect(self.showErrorMessage)
-        if self.authorization.get_connection():
+        self.lingualeo = connect.Lingualeo(login, password, cookies_path)
+        self.lingualeo.Error.connect(self.showErrorMessage)
+        if self.lingualeo.get_connection():
             # Disable login button and fields
             self.set_login_form_enabled(False)
             # Enable all other buttons
@@ -205,9 +244,9 @@ class PluginWindow(QDialog):
             self.set_download_form_enabled(True)
 
     def download_words(self, wordsets=None):
-        # TODO: Test on big dictionaries and check if async run needed
+        # TODO: Run it inside the other thread to handle big dictionaries
         self.allow_to_close(False)
-        words = self.authorization.get_words_to_add(wordsets)
+        words = self.lingualeo.get_words_to_add(wordsets)
         filtered = self.filter_words(words)
         if filtered:
             self.start_download_thread(filtered)
@@ -218,18 +257,35 @@ class PluginWindow(QDialog):
             self.set_download_form_enabled(True)
             self.allow_to_close(True)
 
+    def filter_words(self, words):
+        """
+        Eliminates unnecessary to download words.
+        We need to do it in main thread by using signals and slots
+        """
+        word_progress = self.get_progress_type()
+        if word_progress == 'Unstudied':
+            words = [word for word in words if word.get('progress_percent') < 100]
+        elif word_progress == 'Studied':
+            words = [word for word in words if word.get('progress_percent') == 100]
+        update = self.checkBoxUpdateNotes.checkState()
+        if not update:
+            # Exclude duplicates, if full update is not required
+            words = [word for word in words if not utils.is_duplicate(word)]
+        return words
+
     def start_download_thread(self, words):
         # Activate progress bar
         self.progressBar.setValue(0)
         self.progressBar.show()
         self.progressLabel.show()
         self.logoutButton.setEnabled(False)
+        # TODO: Show numbers in progress bar
 
         # Set Anki Model
         self.set_model()
 
         # Start downloading
-        self.threadclass = Download(words)
+        self.threadclass = connect.Download(words)
         self.threadclass.start()
         self.threadclass.Length.connect(self.progressBar.setMaximum)
         self.threadclass.Word.connect(self.addWord)
@@ -240,6 +296,23 @@ class PluginWindow(QDialog):
 
     def set_model(self):
         self.model = utils.prepare_model(mw.col, utils.fields, styles.model_css)
+
+    def addWord(self, word):
+        """
+        Note is an SQLite object in Anki so you need
+        to fill it out inside the main thread
+        """
+        utils.add_word(word, self.model)
+
+    def setFinalCount(self, counter):
+        self.wordsFinalCount = counter
+
+# UI helpers
+#####################################
+
+    def showErrorMessage(self, msg):
+        showInfo(msg)
+        mw.reset()
 
     def get_progress_type(self):
         progress = 'Any'
@@ -272,61 +345,6 @@ class PluginWindow(QDialog):
         self.checkBoxStayLoggedIn.setEnabled(mode)
         self.checkBoxSavePass.setEnabled(mode)
 
-    def filter_words(self, words):
-        """
-        Eliminates unnecessary to download words.
-        We need to do it in main thread by using signals and slots
-        """
-        word_progress = self.get_progress_type()
-        if word_progress == 'Unstudied':
-            words = [word for word in words if word.get('progress_percent') < 100]
-        elif word_progress == 'Studied':
-            words = [word for word in words if word.get('progress_percent') == 100]
-        update = self.checkBoxUpdateNotes.checkState()
-        if not update:
-            # Exclude duplicates, if full update is not required
-            words = [word for word in words if not utils.is_duplicate(word)]
-        return words
-
-    def addWord(self, word):
-        """
-        Note is an SQLite object in Anki so you need
-        to fill it out inside the main thread
-        """
-        utils.add_word(word, self.model)
-
-    def reject(self):
-        """
-        Override reject event to handle Escape key press correctly
-        """
-        self.close()
-
-    def closeEvent(self, event):
-        """
-        Override close event to safely close plugin
-        """
-        if hasattr(self, 'threadclass') and not self.threadclass.isFinished():
-            qm = QMessageBox()
-            answer = qm.question(self, '', "Are you sure you want to stop downloading?",
-                                 qm.Yes | qm.Cancel, qm.Cancel)
-            if answer == qm.Yes and not self.threadclass.isFinished():
-                self.threadclass.terminate()
-            elif answer == qm.Cancel:
-                event.ignore()
-                return
-        # Delete attribute before closing to allow running the plugin again
-        delattr(mw, 'lingualeoanki')
-        if not self.checkBoxStayLoggedIn.checkState():
-            utils.clean_cookies()
-        mw.reset()
-
-    def setFinalCount(self, counter):
-        self.wordsFinalCount = counter
-
-    def showErrorMessage(self, msg):
-        showInfo(msg)
-        mw.reset()
-
     def allow_to_close(self, flag):
         """
         Sets attribute 'silentlyClose' to allow Anki's main window
@@ -337,17 +355,6 @@ class PluginWindow(QDialog):
             setattr(self, 'silentlyClose', 1)
         elif hasattr(self, 'silentlyClose'):
             delattr(self, 'silentlyClose')
-
-    def downloadFinished(self):
-        if hasattr(self, 'wordsFinalCount'):
-            showInfo("%d words from LinguaLeo have been processed" % self.wordsFinalCount)
-            delattr(self, 'wordsFinalCount')
-        self.set_download_form_enabled(True)
-        self.logoutButton.setEnabled(True)
-        self.progressLabel.hide()
-        self.progressBar.hide()
-        self.allow_to_close(True)
-        mw.reset()
 
 
 class WordsetsWindow(QDialog):
@@ -408,124 +415,3 @@ class WordsetsWindow(QDialog):
         # Send signal to activate buttons and radio buttons on the main plugin window
         self.Cancel.emit(True)
         self.close()
-
-
-# TODO: Move Authorization and Download classes into a different module?
-
-class Authorization(QObject):
-    Error = pyqtSignal(str)
-
-    # TODO: Check if parent=None is needed
-    def __init__(self, login, password, cookies_path, parent=None):
-        QObject.__init__(self, parent)
-        self.login = login
-        self.password = password
-        self.cookies_path = cookies_path
-        self.msg = ''
-
-    def get_connection(self):
-        if not hasattr(self, 'leo'):
-            self.leo = connect.Lingualeo(self.login, self.password, self.cookies_path)
-        try:
-            if not self.leo.is_authorized():
-                status = self.leo.auth()
-                if status['error_msg']:
-                    self.msg = status['error_msg']
-        except requests.exceptions.RequestException:
-            self.msg = "Can't authorize. Check your internet connection."
-        except ValueError:
-            self.msg = "Error! Possibly, invalid data was received from LinguaLeo"
-        except Exception as e:
-            # TODO improve exception handling
-            self.msg = "There's been an unexpected error. Sorry about that! " + str(e.args)
-        if self.msg:
-            self.Error.emit(self.msg)
-            self.msg = ''
-            return False
-        return True
-
-    def get_wordsets(self):
-        if not self.get_connection():
-            return None
-        try:
-            wordsets = self.leo.get_wordsets()
-            if not wordsets:
-                self.msg = 'No user dictionaries found'
-        except (requests.exceptions.RequestException, socket.error):
-            self.msg = "Can't get dictionaries. Check your internet connection."
-        except ValueError:
-            self.msg = "Error! Possibly, invalid data was received from LinguaLeo"
-        if self.msg:
-            self.Error.emit(self.msg)
-            self.msg = ''
-            return None
-        return wordsets
-
-    def get_words_to_add(self, wordsets=None):
-        if not self.get_connection():
-            return None
-        try:
-            words = self.leo.get_words(wordsets)
-        except requests.exceptions.RequestException:
-            self.msg = "Can't download words. Check your internet connection."
-        except ValueError:
-            self.msg = "Error! Possibly, invalid data was received from LinguaLeo"
-
-        if self.msg:
-            self.Error.emit(self.msg)
-            self.msg = ''
-            return None
-
-        return words
-
-
-class Download(QThread):
-    Length = pyqtSignal(int)
-    Counter = pyqtSignal(int)
-    FinalCounter = pyqtSignal(int)
-    Word = pyqtSignal(dict)
-    Error = pyqtSignal(str)
-
-    def __init__(self, words, parent=None):
-        QThread.__init__(self, parent)
-        self.words = words
-
-    def run(self):
-        self.Length.emit(len(self.words))
-        self.add_separately()
-
-    def add_separately(self):
-        """
-        Divides downloading and filling note to different threads
-        because you cannot create SQLite objects outside the main
-        thread in Anki. Also you cannot download files in the main
-        thread because it will freeze GUI
-        """
-        counter = 0
-        problem_words = []
-
-        for word in self.words:
-            self.Word.emit(word)
-            try:
-                utils.send_to_download(word, self)
-            except (urllib.error.URLError, socket.error):
-                problem_words.append(word.get('word_value'))
-            counter += 1
-            # TODO: Show numbers in progress bar
-            self.Counter.emit(counter)
-        self.FinalCounter.emit(counter)
-
-        # TODO: save problem words in json format to user_files folder
-        #  and ask user to retry downloading problem words
-
-        if problem_words:
-            self.problem_words_msg(problem_words)
-
-    def problem_words_msg(self, problem_words):
-        error_msg = ("We weren't able to download media for these "
-                     "words because of broken links in LinguaLeo "
-                     "or problems with an internet connection: ")
-        for problem_word in problem_words[:-1]:
-            error_msg += problem_word + ', '
-        error_msg += problem_words[-1] + '.'
-        self.Error.emit(error_msg)
